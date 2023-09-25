ce46b78f28209240287dfac5a284c86f8d03624d
Mohammad Yazdian <m.yazdian70@outlook.com>
2023-09-25 14:20:44 +0330

    Initialize project

---
+++ src/main/java/ir/co/holoo/discovery/HolooDiscoveryServerApplication.java
@@ -0,0 +1,15 @@
+package ir.co.holoo.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class HolooDiscoveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolooDiscoveryServerApplication.class, args);
+    }
+
+}
